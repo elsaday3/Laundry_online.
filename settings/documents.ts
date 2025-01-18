89f210bced8c3a6a092baba85668d518ce4b00f9
elsaday3 <elsadayjuntak3@gmail.com>
2025-01-19 01:09:13 +0700

    ubah

--- settings/documents.ts
+++ settings/documents.ts
@@ -63,7 +63,7 @@ export const Documents: Paths[] = [
     heading: "Dokumen design",
     items: [
       {
-        title: "Cards",
+        title: "Sketsa Awal",
         href: "/cards",
       },
       {
